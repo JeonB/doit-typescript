2acc83a212a8d6e705c26dd5ac66a52b7e42e4ac
JeonB <newjeonb@gmail.com>
2024-03-15 21:12:12 +0900

    Complete Chapter 2

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-import { testMakePerson } from "./utils/makePerson";
+import { Chance } from "chance";
+import IPerson from "./person/IPerson";
+import Person, { makePerson } from "./person/Person";
+import * as R from "ramda";
 
-testMakePerson();
+const chance = new Chance();
+let persons: IPerson[] = R.range(0, 2).map(
+  (n: number) => new Person(chance.name(), chance.age())
+);
+console.log(persons);
+
+// const testMakePerson = (): void => {
+//   let jane: IPerson = makePerson("Jane");
+//   let jack: IPerson = new Person("Jack");
+//   console.log(jane, jack);
+// };
+
+// testMakePerson();
